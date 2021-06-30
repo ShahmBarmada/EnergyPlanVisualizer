dc6bae467478a08d369c6794f936721f3f62646c
Shahm Barmada <barmada.sha@gmail.com>
2021-06-30 18:53:36 +0200

    added combobox item

--- Stage3.py
+++ Stage3.py
@@ -1,7 +1,7 @@
+import tkinter, csv
 from tkinter import *
 from tkinter import ttk, filedialog
 import pandas as pd
-import csv
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
@@ -18,3 +18,15 @@ aliasList = list(aliasDict.values())
 headerList = list(aliasDict.keys())
 
 plt.close()
+
+window = tkinter.Tk()
+window.title('EnergyPlanVisualizer Stage3')
+window.geometry('1010x200')
+
+comboSelection = tkinter.StringVar()
+comboField = ttk.Combobox(window, width=40, textvariable=comboSelection)
+comboField['values'] = aliasList
+comboField.grid(column=0, row=1, padx=10, pady=10)
+comboField.current()
+
+window.mainloop()
