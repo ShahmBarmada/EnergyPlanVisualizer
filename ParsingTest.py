b359bc601e354182096a32c8d25d85598bed66e4
Shahm Barmada <45445736+ShahmBarmada@users.noreply.github.com>
2021-07-01 02:55:10 +0200

    edited to output a plot file

--- ParsingTest.py
+++ ParsingTest.py
@@ -1,6 +1,6 @@
-import tkinter as tk
+import tkinter
 from tkinter import *
-from tkinter import filedialog, ttk
+from tkinter import filedialog
 
 import pandas as pd
 import csv
@@ -8,12 +8,8 @@ import csv
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
-#rootWindow = tkinter.Tk()
-#rootWindow.withdraw()
-
-window = tk.Tk()
-window.title('Test App')
-window.geometry('1010x200')
+rootWindow = tkinter.Tk()
+rootWindow.withdraw()
 
 inPath = filedialog.askopenfilename()
 outPath = inPath[:inPath.rfind('/')] + '/test.png'
@@ -27,32 +23,30 @@ with open(headerPath, 'r') as ipf:
 aliasList = list(aliasDict.values())
 headerList = list(aliasDict.keys())
 
-#fig, ax = plt.subplots()
-#ax.set_xlabel('Hourly Values')
-#ax.set_ylabel('Electric Demand')
-#ax.set_title('Electric Demand Jan, Feb, Mar')
-#
-#xAxis = StudyDF.loc[143:311,'g0-Data1']
-#yAxis = StudyDF.loc[143:311,'g2-ElectrDemand']
-#ax.plot(xAxis, yAxis)
-#
-#xAxis = StudyDF.loc[143:311,'g0-Data1']
-#yAxis = StudyDF.loc[816:984,'g2-ElectrDemand']
-#ax.plot(xAxis, yAxis)
-#
-#xAxis = StudyDF.loc[143:311,'g0-Data1']
-#yAxis = StudyDF.loc[1487:1655,'g2-ElectrDemand']
-#ax.plot(xAxis, yAxis)
-#
-#ax.legend(['January','February','March'])
-#ax.xaxis.set_major_locator(MultipleLocator(24))
-#ax.xaxis.set_minor_locator(MultipleLocator(12))
-#ax.yaxis.set_major_locator(MultipleLocator(5000))
-#
-#fig.set_size_inches(18.5,10.5)
-#
-#plt.savefig(outpath)
+fig, ax = plt.subplots()
+ax.set_xlabel('Hourly Values')
+ax.set_ylabel('Electric Demand')
+ax.set_title('Electric Demand Jan, Feb, Mar')
 
-#rootWindow.destroy()
+xAxis = StudyDF.loc[143:311,'g0-Data1']
+yAxis = StudyDF.loc[143:311,'g2-ElectrDemand']
+ax.plot(xAxis, yAxis)
 
-window.mainloop()
+xAxis = StudyDF.loc[143:311,'g0-Data1']
+yAxis = StudyDF.loc[816:984,'g2-ElectrDemand']
+ax.plot(xAxis, yAxis)
+
+xAxis = StudyDF.loc[143:311,'g0-Data1']
+yAxis = StudyDF.loc[1487:1655,'g2-ElectrDemand']
+ax.plot(xAxis, yAxis)
+
+ax.legend(['January','February','March'])
+ax.xaxis.set_major_locator(MultipleLocator(24))
+ax.xaxis.set_minor_locator(MultipleLocator(12))
+ax.yaxis.set_major_locator(MultipleLocator(5000))
+
+fig.set_size_inches(18.5,10.5)
+
+plt.savefig(outPath)
+
+rootWindow.destroy()
