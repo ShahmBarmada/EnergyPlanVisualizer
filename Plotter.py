26e0e93e9c6e7b58ce98ec7df8d04bdc6b0c96cc
Shahm Barmada <barmada.sha@gmail.com>
2021-07-27 18:09:56 +0200

    Update Plotter.py

--- Plotter.py
+++ Plotter.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly.graph_objects as plygo
+from plotly.subplots import make_subplots
 
 def plotter (srcFig = dict, srcPlt = list) -> list:
     fig = srcFig['id']
@@ -6,4 +8,30 @@ def plotter (srcFig = dict, srcPlt = list) -> list:
     for i in range(0, len(srcPlt)):
         plt.append(srcPlt[i]['id'])
 
+    fig = make_subplots(rows= srcFig['rows'], cols= srcFig['cols'])
+
+    for i in range(0, len(srcPlt)):
+        if srcPlt[i]['tracetype'] == 'Scatter':
+            pass
+
+        elif srcPlt[i]['tracetype'] == 'Bar':
+            pass
+
+        elif srcPlt[i]['tracetype'] == 'Pie':
+            pass
+
     return [fig, plt]
+
+# make subplot (rows, cols, print_grid, subplot_titles, specs, insets, column_titles, row_titles, x_title, y_title)
+
+# update layout (title, height, width, legend, plot_bgcolor)
+
+# add_trace
+# scatter (x, y, mode{markers, lines+markers, lines}, connectgaps, line_shape{spline, linear}, fill{tonexty, none}, name)
+# scatter layout (title, xaxis{tickmode, tick0, dtick}, yaxis{tickmode, tick0, dtick}, xaxis_tickformat, yaxis_tickformat )
+#
+# Bar (x, y, barmode{gourp, stack, relative}, opacity, textposition{auto, outside}, bargap, bargroupgap)
+# Bar layout ()
+#
+# Pie (labels, values, hole, pull, )
+# Pie layout ()
