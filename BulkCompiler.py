dc1f6aa2ee7b9be4f743ab02ab02a085c248eaf7
Shahm Barmada <45445736+ShahmBarmada@users.noreply.github.com>
2021-09-24 08:49:01 +0200

    Update BulkCompiler.py

--- BulkCompiler.py
+++ BulkCompiler.py
@@ -2,13 +2,10 @@ import pandas as pd
 import plotly.graph_objects as plygo
 import plotly.io as pio
 from plotly.subplots import make_subplots
-from functools import reduce
 
 def EnergyBalance (stdCollection = list):
 
-    BulkDF = pd.DataFrame(0, index=['Coal', 'Oil', 'Biomass', 'Renewable'], columns=['test']
-    #columns=['g1-DHP','g1-CHP2','g1-CHP3','g1-Boiler2','g1-Boiler3','g1-PP','g1-Geo/Nu','g1-Hydro','g1-Waste/HTL','g1-CAES/ELT','g1-BioCon','g1-EFuel','g1-VRES','g1-SolarTh','g1-Transp','g1-Househ','g1-Ind/Var'])
-    )
+    BulkDF = pd.DataFrame(0, index=['Coal', 'Oil', 'Biomass', 'Renewable', 'H2Etc.', 'Biofuel', 'Nucl/Ccs'], columns=['test'])
 
     for path in range(len(stdCollection)):
         stdDF = pd.read_csv(stdCollection[path], delimiter=',', low_memory=False, index_col='Index')
@@ -16,25 +13,17 @@ def EnergyBalance (stdCollection = list):
         rangeStart = stdDF.index.get_loc('TotalAnnualCosts')
         rangeStart = rangeStart + stdDF.iloc[rangeStart:rangeStart+100].index.get_loc('Coal')
                 
-        rangeEnd = stdDF.iloc[rangeStart:rangeStart+100].index.get_loc('Renewable')
+        rangeEnd = stdDF.iloc[rangeStart:rangeStart+100].index.get_loc('Nucl/Ccs')
         rangeEnd = rangeEnd + rangeStart +1
 
         xData = stdDF.iloc[rangeStart:rangeEnd].index.values.tolist()
         
-#        yData = []
-
-#        for yData_i, headers in enumerate(list(stdDF.columns.values)):
-#            if headers[:2] == 'g1':
-#                yData.append(headers)
-#        yData.pop()
-
         stdDF = stdDF.iloc[rangeStart:rangeEnd]
         stdDF = stdDF.loc[:, 'g1-Total']
 
         BulkDF.insert(0, 'std' + str(path), stdDF)
 
-#        BulkDF = reduce(lambda x, y: x.add(y, fill_value=0), [BulkDF, stdDF])
-    print(stdDF)
+    BulkDF.drop(['test'], axis= 1, inplace=True)
     print(BulkDF)
 
     figure = make_subplots()
